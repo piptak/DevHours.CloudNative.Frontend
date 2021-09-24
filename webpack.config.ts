fb6d00f549ca39b504b052334ea7fd85a1b7cd5b
Jaroslaw Paradysz <jaroslaw.a.paradysz@capgemini.com>
2021-09-24 20:58:15 +0200

    Reducing bundle size step 3

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
-import { Configuration as WebpackConfiguration, DefinePlugin } from "webpack";
+import { Configuration as WebpackConfiguration, ContextReplacementPlugin, DefinePlugin } from "webpack";
 import WebpackDevServer from "webpack-dev-server";
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
 interface Configuration extends WebpackConfiguration {
     devServer?: WebpackDevServer.Configuration;
@@ -63,6 +64,8 @@ const webpackConfig = (): Configuration => ({
     historyApiFallback: true,
   },
   plugins: [
+    //new BundleAnalyzerPlugin(),
+    new ContextReplacementPlugin(/moment[/\\]locale$/, /pl|en|nl/),
     new HtmlWebpackPlugin({
       // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
       template: "./public/index.html",
